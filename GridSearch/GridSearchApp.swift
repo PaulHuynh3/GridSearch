ff17e2880730d502c60db083dd7d563cfebd6496
Paul <paul.huynh3@gmail.com>
2023-10-05 13:06:45 -0400

    Initial Commit

---
+++ GridSearch/GridSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchApp.swift
+//  GridSearch
+//
+//  Created by Paul Huynh on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
