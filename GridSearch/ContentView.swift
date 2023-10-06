df1a63e8a3d00e6ab582369136f654930e4b530a
Paul <paul.huynh3@gmail.com>
2023-10-05 22:38:13 -0400

    Update swift concurrency

--- GridSearch/ContentView.swift
+++ GridSearch/ContentView.swift
@@ -72,8 +72,9 @@ class GridViewModel: ObservableObject {
             let (data, _) = try await URLSession.shared.data(from:URL(string: "https://rss.applemarketingtools.com/api/v2/us/apps/top-free/50/apps.json")!)
             
             let rss = try JSONDecoder().decode(RSS.self, from: data)
-            self.results = rss.feed.results
-            
+            await MainActor.run {
+                self.results = rss.feed.results
+            }
         } catch {
             print("failred to reach endpoint \(error)")
         }
